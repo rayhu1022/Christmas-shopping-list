a03f5dbb2cebec707123d482f0271f27b768578f
Ray Hu <yunghu001@gmail.com>
2022-10-19 19:33:23 -0700

    fixed typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ exteded: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/', homeRoutes)
