25d8ac5c7d6f2435fde4a246a9fce1637708fa49
Ray Hu <yunghu001@gmail.com>
2022-11-08 17:13:40 -0800

    fixed logout error

--- routes/xmas.js
+++ routes/xmas.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const xmasController = require('../controllers/xmas')
+const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', xmasController.getGifts)
+router.get('/', ensureAuth, xmasController.getGifts)
 router.post('/createGift', xmasController.createGift)
 router.put('/markComplete', xmasController.markComplete)
 router.put('/markIncomplete', xmasController.markIncomplete)
